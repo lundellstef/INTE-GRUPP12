1aeea516958c3ac1f3b6bc182c2b9a5588431519
Stefan Lundell <45182915+lundellstef@users.noreply.github.com>
2023-09-01 11:06:44 +0200

    add gitignore and tests

---
+++ src/main/java/com/inte/examples/Thing.java
@@ -0,0 +1,12 @@
+package com.inte.examples;
+
+public class Thing {
+
+    public Thing() {
+
+    }
+
+    double convertCelsiusToFahrenheit(double celsius) {
+        return 9.0/5 * celsius + 32;
+    }
+}
