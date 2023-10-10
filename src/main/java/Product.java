7c019d55ca951ee1fc6e3b13a0cac14ddf45f0de
Stefan Lundell <45182915+lundellstef@users.noreply.github.com>
2023-10-10 13:39:38 +0200

    Product & TestProduct progress

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,25 +1,37 @@
 public abstract class Product {
 
-    private int price;
+    private int priceInMinorUnits;
     private String productID;
     private int discount;
     private boolean hasDiscount;
     private VAT vatRate;
 
+    /**
+     * Returns the total price of the product including VAT.
+     * If a banana costs 2000 without VAT (20 SEK), this would return 2240.
+     **/
     public int getPriceWithVat() {
-        return (getPrice() * vatRate.value) / 100;
+        return (getPrice() * (getVat().value + 100) / 100);
     }
 
     /**
-     * Price is entered in minor units, WITHOUT tax.
+     * Returns the amount of VAT from the total price.
+     * If a banana costs 2240 with VAT, this would return 240.
+     */
+    public int getVatAmount() {
+        return getPriceWithVat() - getPrice();
+    }
+
+    /**
+     * Price is entered in minor units, WITHOUT VAT.
      * If a banana costs 20 SEK, the price entered would be 2000. (2000 öre.)
      **/
     public int getPrice() {
-        return price;
+        return priceInMinorUnits;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setPrice(int priceInMinorUnits) {
+        this.priceInMinorUnits = priceInMinorUnits;
     }
 
     public String getProductID() {
--- src/test/java/TestProduct.java
+++ src/test/java/TestProduct.java
@@ -1,9 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestProduct {
 
     static final String DEFAULT_PRODUCT_ID = "Banana";
     static final int DEFAULT_PRODUCT_PRICE = 2000;
+    static final int DEFAULT_PRODUCT_PRICE_WITH_VAT12 = 2240;
+    static final int DEFAULT_VAT_AMOUNT_WITH_VAT12 = 240;
     static final int DEFAULT_PRODUCT_DISCOUNT = 10;
     static final int NO_DISCOUNT = 0;
+
+    @Test
+    void should_returnCorrectPriceWithVat_when_productIsCreated() {
+        ProductItem item = createDefaultProductItem();
+        assertEquals(item.getPriceWithVat(), DEFAULT_PRODUCT_PRICE_WITH_VAT12);
+    }
+
+    @Test
+    void should_returnCorrectVatAmount_when_productIsCreated() {
+        ProductItem item = createDefaultProductItem();
+        assertEquals(item.getVatAmount(), DEFAULT_VAT_AMOUNT_WITH_VAT12);
+    }
 
     private ProductItem createDefaultProductItem() {
         return new ProductItem(DEFAULT_PRODUCT_PRICE, DEFAULT_PRODUCT_ID, NO_DISCOUNT, VAT.FOOD);
