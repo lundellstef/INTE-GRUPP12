c0b6aec98facd4b5ecfec16e525f3a5cb2d51821
Stefan Lundell <45182915+lundellstef@users.noreply.github.com>
2023-10-10 11:02:47 +0200

    Basic product setup

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,14 @@
-public class Product {
+public abstract class Product {
+
+    private int price;
+    private String productID;
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
 }
