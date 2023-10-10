7563a8c1c5e23088f97375f9625c1acd386fa72e
Stefan Lundell <45182915+lundellstef@users.noreply.github.com>
2023-10-10 13:17:40 +0200

    TestProduct setup

--- src/test/java/TestProduct.java
+++ src/test/java/TestProduct.java
@@ -1,3 +1,12 @@
 public class TestProduct {
 
+    static final String DEFAULT_PRODUCT_ID = "Banana";
+    static final int DEFAULT_PRODUCT_PRICE = 2000;
+    static final int DEFAULT_PRODUCT_DISCOUNT = 10;
+    static final int NO_DISCOUNT = 0;
+
+    private ProductItem createDefaultProductItem() {
+        return new ProductItem(DEFAULT_PRODUCT_PRICE, DEFAULT_PRODUCT_ID, NO_DISCOUNT, VAT.FOOD);
+    }
+
 }
