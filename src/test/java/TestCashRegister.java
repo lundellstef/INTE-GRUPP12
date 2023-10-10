559b0ffb260817d39ef4a727abacdcf1292b4d6a
minnaq <minnaq@kth.se>
2023-10-10 20:59:27 +0200

    another test added to TestCashRegister

--- src/test/java/TestCashRegister.java
+++ src/test/java/TestCashRegister.java
@@ -3,7 +3,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCashRegister {
 
     String DEFAULT_PRODUCT_ID = "123";
@@ -11,7 +16,7 @@ public class TestCashRegister {
     int DEFAULT_PRODUCT_ITEM_PRICE = 2000;
     int DEFAULT_DISCOUNT = 10;
 
-    int DEFAULT_PRICE_INCREASE = 2250;
+    int DEFAULT_TOTAL_PRICE = 2250;
 
     VAT DEFAULT_VAT = VAT.STANDARD;
 
@@ -21,16 +26,38 @@ public class TestCashRegister {
         Purchase purchase = new Purchase();
         ProductItem productItem = new ProductItem(DEFAULT_PRODUCT_ITEM_PRICE, DEFAULT_PRODUCT_ID, DEFAULT_DISCOUNT, DEFAULT_VAT);
         purchase.scanItem(productItem);
-        assertEquals(DEFAULT_PRICE_INCREASE, cashRegister.getTotalAmount());
+        assertEquals(DEFAULT_TOTAL_PRICE, cashRegister.getTotalAmount());
     }
 
     @Test
-    void totalAmountIsCorrectAfterPurchaseHasBeenMadeAndTotalAmountIsNotZero(){
+    void totalAmountIsCorrectAfterPurchaseHasBeenMadeAndTotalAmountWasNotZero(){
         CashRegister cashRegister = new CashRegister();
         cashRegister.setTotalAmount(DEFAULT_TOTAL_AMOUNT_ABOVE_ZERO);
         Purchase purchase = new Purchase();
         ProductItem productItem = new ProductItem(DEFAULT_PRODUCT_ITEM_PRICE, DEFAULT_PRODUCT_ID, DEFAULT_DISCOUNT, DEFAULT_VAT);
         purchase.scanItem(productItem);
-        assertEquals(DEFAULT_TOTAL_AMOUNT_ABOVE_ZERO + DEFAULT_PRICE_INCREASE, cashRegister.getTotalAmount());
+        assertEquals(DEFAULT_TOTAL_AMOUNT_ABOVE_ZERO + DEFAULT_TOTAL_PRICE, cashRegister.getTotalAmount());
     }
+
+    @Test
+    void throwsExceptionWhenMoneyForChangeDoesNotExist(){
+
+        List<Money> customerPayment = new ArrayList<>();
+        customerPayment.add(new Money(SEK, 1_000));
+        customerPayment.add(new Money(SEK, 1_000));
+        customerPayment.add(new Money(SEK, 500));
+
+        List<Money> moneyInRegister = new ArrayList<>();
+        moneyInRegister.add(new Money(SEK, 1_000));
+        moneyInRegister.add(new Money(SEK, 1_000));
+        moneyInRegister.add(new Money(SEK, 1_000));
+
+        CashRegister cashRegister = new CashRegister();
+        cashRegister.setMoneyInRegister(moneyInRegister);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            cashRegister.handlePayment(customerPayment, DEFAULT_TOTAL_PRICE);
+        });
+    }
+
 }
