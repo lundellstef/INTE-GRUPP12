2c0d7fddb6a797381481cafcff8165e97af972ba
Stefan Lundell <45182915+lundellstef@users.noreply.github.com>
2023-10-10 10:50:30 +0200

    Basic VAT setup
    Created as an enum

--- src/main/java/VAT.java
+++ src/main/java/VAT.java
@@ -1,2 +1,10 @@
-public class VAT {
+// The toll rates are: 25%, 12%, 6%, 0%.
+public enum VAT {
+    STANDARD(25), REDUCED(12), LOW(6), TOLL_FREE(0);
+
+    public final int percentageValue;
+
+    VAT(int value) {
+        this.percentageValue = value;
+    }
 }
