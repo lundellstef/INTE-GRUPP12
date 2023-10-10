ceb6b59213f1c18621b46f293db597bc6d268186
minnaq <minnaq@kth.se>
2023-10-10 14:06:27 +0200

    getVat method call in Purchase changed

--- src/test/java/TestCashRegister.java
+++ src/test/java/TestCashRegister.java
@@ -1,2 +1,13 @@
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestCashRegister {
+
+    @Test
+    void should_returnCorrectTotalAmount_when_purchaseHasBeenMadeAndTotalAmountIsZero(){
+
+    }
 }
