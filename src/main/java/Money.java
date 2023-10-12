af86fc29afa738726d373ce8959f85915123bdfa
minnaq <minnaq@kth.se>
2023-10-12 11:11:42 +0200

    TDD for Money

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,10 +1,21 @@
 public class Money {
 
-
-    public Money(String currency, int amount){
-        if(amount < 0 || !currency.equals("SEK")) {
+    long amountInMinorUnit;
+    String currency = "SEK";
+    Money(int amountInMinorUnit){
+        if(amountInMinorUnit < 0) {
             throw new IllegalArgumentException();
         }
+        this.amountInMinorUnit = amountInMinorUnit;
     }
+
+    public long getAmountInMinorUnit(){
+        return amountInMinorUnit;
+    }
+
+    public String getCurrency(){
+        return currency;
+    }
+
 
 }
--- src/test/java/TestMoney.java
+++ src/test/java/TestMoney.java
@@ -3,29 +3,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class TestMoney {
 
-    static final String VALID_CURRENCY = "SEK";
-    static final String INVALID_CURRENCY = "USD";
+    static final String DEFAULT_CURRENCY = "SEK";
     static final int VALID_AMOUNT = 100;
     static final int INVALID_AMOUNT = -100;
 
     @Test
     public void throwsExceptionWhenAmountIsLessThanZero(){
         assertThrows(IllegalArgumentException.class, () -> {
-            new Money(VALID_CURRENCY, INVALID_AMOUNT);
+            new Money(INVALID_AMOUNT);
         });
     }
 
     @Test
-    public void throwsExceptionWhenCurrencyIsSomethingOtherThanSEK(){
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Money(INVALID_CURRENCY, VALID_AMOUNT);
-        });
+    public void getAmountReturnsCorrectAmount(){
+        Money money = new Money(VALID_AMOUNT);
+        long amount = money.getAmountInMinorUnit();
+        assertEquals(100, amount);
     }
 
     @Test
     public void getCurrencyReturnsCorrectCurrency(){
-        Money money = new Money(VALID_CURRENCY, VALID_AMOUNT);
-        assertEquals("SEK", money.getCurrency());
+        Money money = new Money(VALID_AMOUNT);
+        String currency = money.getCurrency();
+        assertEquals(DEFAULT_CURRENCY, currency);
     }
+
+
 
 }
