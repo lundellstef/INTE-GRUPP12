686e0749eafe3c8ddf5ef20edb2287c51b02e4c1
minnaq <minnaq@kth.se>
2023-10-14 15:25:42 +0200

    Added txt-file for amountOfMoneyInStore and created constructor to read from it

--- src/main/java/CashRegister.java
+++ src/main/java/CashRegister.java
@@ -1,3 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class CashRegister {
 
+    Money amountOfMoneyInStore;
+    public CashRegister(){
+        String moneyDatabase = "src/main/java/cashRegisterMoney.txt";
+        try {
+            FileReader fileReader = new FileReader(moneyDatabase);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String readLine = reader.readLine();
+            if(readLine == null){
+                amountOfMoneyInStore = new Money(0);
+            } else {
+                amountOfMoneyInStore = new Money(Long.parseLong(readLine));
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public long getAmountOfMoneyInStore(){
+        return amountOfMoneyInStore.getAmountInMinorUnit();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/TestCashRegister.java
+++ src/test/java/TestCashRegister.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCashRegister {
+
+    long VALID_AMOUNT_READ_FROM_DATABASE_FILE = 200_000;
+
+    @Test
+    public void cashRegisterAmountOfMoneyCorrectlyReadFromDatabaseFile(){
+        CashRegister cashRegister = new CashRegister();
+        long amountOfMoneyInStore = cashRegister.getAmountOfMoneyInStore();
+        assertEquals(VALID_AMOUNT_READ_FROM_DATABASE_FILE, amountOfMoneyInStore);
+    }
+
 }
