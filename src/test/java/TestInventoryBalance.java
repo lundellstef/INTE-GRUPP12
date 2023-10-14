ee44a5bf84565d42c8f1564e91d94d5f2477d146
Stefan Lundell <45182915+lundellstef@users.noreply.github.com>
2023-10-14 15:38:21 +0200

    Initial tests for InventoryBalance (TDD)

--- src/test/java/TestInventoryBalance.java
+++ src/test/java/TestInventoryBalance.java
@@ -1,29 +1,56 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestInventoryBalance {
 
+    static final int VALID_AMOUNT_TO_INCREASE_BY = 5;
+    static final int VALID_AMOUNT_TO_DECREASE_BY = -10;
+
     @Test
-    void increasesCountOfItem_when_tryingToAddItemAlreadyPresentInInventory() {
+    void increasesCountOfItem_when_tryingToIncreaseCountByValidAmount() {
+        InventoryBalance ib = createDefaultInventoryBalanceForTesting();
+        Product itemInInventory = createDefaultItemForTesting();
+
+        int previousCountOfItem = ib.getCount(itemInInventory);
+
+        ib.updateCount(itemInInventory, VALID_AMOUNT_TO_INCREASE_BY);
+        int currentCountOfItem = ib.getCount(itemInInventory);
+
+        assertEquals(currentCountOfItem, previousCountOfItem + VALID_AMOUNT_TO_INCREASE_BY);
+    }
+
+    @Test
+    void decreaseCountOfItem_when_tryingToDecreaseCountByValidAmount() {
+        InventoryBalance ib = createDefaultInventoryBalanceForTesting();
+        Product itemInInventory = createDefaultItemForTesting();
+
+        int previousCountOfItem = ib.getCount(itemInInventory);
+
+        ib.updateCount(itemInInventory, VALID_AMOUNT_TO_DECREASE_BY);
+        int currentCountOfItem = ib.getCount(itemInInventory);
+
+        assertEquals(currentCountOfItem, previousCountOfItem + VALID_AMOUNT_TO_DECREASE_BY);
+    }
+
+    @Test
+    void throwsException_when_tryingToIncreaseCountOfItemNotInInventory() {
+        InventoryBalance ib = createDefaultInventoryBalanceForTesting();
+        Product itemNotInInventory = createDefaultItemForTestingNotPresentInInventory();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ib.updateCount(itemNotInInventory, VALID_AMOUNT_TO_INCREASE_BY);
+        });
 
     }
 
     @Test
-    void throwsException_when_tryingToAddNegativeAmountOfItem() {
+    void throwsException_when_tryingToDecreaseCountOfItemNotInInventory() {
+        InventoryBalance ib = createDefaultInventoryBalanceForTesting();
+        Product itemNotInInventory = createDefaultItemForTestingNotPresentInInventory();
 
-    }
-
-    @Test
-    void displaysCountOfEveryItem_when_totalInventoryIsPrinted() {
-
-    }
-
-    @Test
-    void throwsException_when_tryingToIncrementItemNotInInventory() {
-
-    }
-
-    @Test
-    void throwsException_when_tryingToDecrementItemNotInInventory() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            ib.updateCount(itemNotInInventory, VALID_AMOUNT_TO_DECREASE_BY);
+        });
 
     }
 
@@ -34,6 +61,10 @@ public class TestInventoryBalance {
 
     @Test
     void decreasesCountInInventory_when_itemIsDecremented() {
+
+    }
+    @Test
+    void displaysCountOfEveryItem_when_totalInventoryIsPrinted() {
 
     }
 }
